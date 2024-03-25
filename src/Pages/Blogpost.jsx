77b465f74614bea06d70590c889972a01dace744
mabgcm <bugucam@gmail.com>
2024-03-25 16:51:21 -0400

    SEO with helmet-react

--- src/Pages/Blogpost.jsx
+++ src/Pages/Blogpost.jsx
@@ -3,6 +3,7 @@ import '../assets/css/main.css';
 import '../assets/css/variables.css';
 import CircleIcon from '@mui/icons-material/Circle';
 import { useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet'; // Import Helmet
 import { UserAuth } from '../context/AuthContext';
 import { doc, onSnapshot, getDocs, collection, query, where } from 'firebase/firestore';
 import { db } from "../firebase";
@@ -63,72 +64,86 @@ const Blogpost = () => {
     return (
         <main id="main">
             {article && (
-                <section className="single-post-content">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-9 col-lg-9 post-content">
-                                <div className="single-post">
-                                    {/* Render the first image at the top if it exists */}
-                                    {article.paragraphs[0].image && (
-                                        <figure className="my-4">
-                                            {/* Ensure that the link property exists and is not empty */}
-                                            {article.paragraphs[0].link ? (
-                                                <a href={article.paragraphs[0].link} target="_blank" rel="noopener noreferrer">
+                <>
+                    <Helmet>
+                        <title>{article.title} - Your Blog Name</title>
+                        <meta name="description" content={article.paragraphs[0].text.substring(0, 150)} />
+                        {/* Add more meta tags as needed */}
+                        <link rel="canonical" href={`https://www.yourblog.com/blogpost/${id}`} />
+                        {/* Open Graph tags for social media */}
+                        <meta property="og:title" content={article.title} />
+                        <meta property="og:description" content={article.paragraphs[0].text.substring(0, 150)} />
+                        <meta property="og:image" content={article.paragraphs[0].image} />
+                        {/* <meta property="og:url" content={`https://www.yourblog.com/blogpost/${id}`} /> */}
+                        <meta property="og:type" content="article" />
+                    </Helmet>
+                    <section className="single-post-content">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-9 col-lg-9 post-content">
+                                    <div className="single-post">
+                                        {/* Render the first image at the top if it exists */}
+                                        {article.paragraphs[0].image && (
+                                            <figure className="my-4">
+                                                {/* Ensure that the link property exists and is not empty */}
+                                                {article.paragraphs[0].link ? (
+                                                    <a href={article.paragraphs[0].link} target="_blank" rel="noopener noreferrer">
+                                                        <img src={article.paragraphs[0].image} alt="" className="img-fluid" />
+                                                    </a>
+                                                ) : (
                                                     <img src={article.paragraphs[0].image} alt="" className="img-fluid" />
-                                                </a>
-                                            ) : (
-                                                <img src={article.paragraphs[0].image} alt="" className="img-fluid" />
-                                            )}
-                                        </figure>
-                                    )}
-                                    <div className="d-flex">
-                                        <div className="col-10 ">
-                                            <div className="post-meta">
-                                                <span className="date">{article.category}</span>
-                                                <span className="mx-1"><CircleIcon fontSize='xsmall' /></span>
-                                                <span>{new Date(article.timestamp?.toDate()).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: '2-digit' }).toUpperCase()}</span>
+                                                )}
+                                            </figure>
+                                        )}
+                                        <div className="d-flex">
+                                            <div className="col-10 ">
+                                                <div className="post-meta">
+                                                    <span className="date">{article.category}</span>
+                                                    <span className="mx-1"><CircleIcon fontSize='xsmall' /></span>
+                                                    <span>{new Date(article.timestamp?.toDate()).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: '2-digit' }).toUpperCase()}</span>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className="col-2">
-                                            <div className=' d-flex'>
-                                                {user && <Likes id={article.id} likes={article.likes} />}
-                                                <div className="pe-2">
-                                                    <p>{article.likes?.length} likes</p>
+                                            <div className="col-2">
+                                                <div className=' d-flex'>
+                                                    {user && <Likes id={article.id} likes={article.likes} />}
+                                                    <div className="pe-2">
+                                                        <p>{article.likes?.length} likes</p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <h1 className="mb-5">{article.title}</h1>
-                                    {/* Render the rest of the paragraphs, subheadings, and images */}
-                                    {article.paragraphs.map((para, index) => (
-                                        <React.Fragment key={index}>
-                                            {index !== 0 && para.subheading && <h2>{para.subheading}</h2>}
-                                            {/* Use dangerouslySetInnerHTML to render the CKEditor content */}
-                                            <div dangerouslySetInnerHTML={createMarkup(para.text)}></div>
-                                            {index !== 0 && para.image && (
-                                                <figure className="my-4">
-                                                    {/* Ensure that the link property exists and is not empty */}
-                                                    {para.link ? (
-                                                        <a href={para.link} target="_blank" rel="noopener noreferrer">
+                                        <h1 className="mb-5">{article.title}</h1>
+                                        {/* Render the rest of the paragraphs, subheadings, and images */}
+                                        {article.paragraphs.map((para, index) => (
+                                            <React.Fragment key={index}>
+                                                {index !== 0 && para.subheading && <h2>{para.subheading}</h2>}
+                                                {/* Use dangerouslySetInnerHTML to render the CKEditor content */}
+                                                <div dangerouslySetInnerHTML={createMarkup(para.text)}></div>
+                                                {index !== 0 && para.image && (
+                                                    <figure className="my-4">
+                                                        {/* Ensure that the link property exists and is not empty */}
+                                                        {para.link ? (
+                                                            <a href={para.link} target="_blank" rel="noopener noreferrer">
+                                                                <img src={para.image} alt="" className="img-fluid" />
+                                                            </a>
+                                                        ) : (
                                                             <img src={para.image} alt="" className="img-fluid" />
-                                                        </a>
-                                                    ) : (
-                                                        <img src={para.image} alt="" className="img-fluid" />
-                                                    )}
-                                                    {para.caption && <figcaption>{para.caption}</figcaption>}
-                                                </figure>
-                                            )}
-                                        </React.Fragment>
-                                    ))}
+                                                        )}
+                                                        {para.caption && <figcaption>{para.caption}</figcaption>}
+                                                    </figure>
+                                                )}
+                                            </React.Fragment>
+                                        ))}
+                                    </div>
+                                    <div className="comments">
+                                        <Comment key={article.id} id={article.id} />
+                                    </div>
                                 </div>
-                                <div className="comments">
-                                    <Comment key={article.id} id={article.id} />
-                                </div>
+                                <Recommend recommendedArticles={recommendedArticles} />
                             </div>
-                            <Recommend recommendedArticles={recommendedArticles} />
                         </div>
-                    </div>
-                </section>
+                    </section>
+                </>
             )}
         </main>
     )
