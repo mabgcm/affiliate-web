92a81bdd34c4718742f90272e1ec794e508af2d8
mabgcm <bugucam@gmail.com>
2024-04-05 04:23:57 -0400

    fetching all lesson plans

--- src/Components/CourseBox.jsx
+++ src/Components/CourseBox.jsx
@@ -1,46 +1,38 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 
-import course from '../assets/img/img/coursee.jpg';
+import courseImage from '../assets/img/img/coursee.jpg';
 
-const CourseBox = () => {
+const CourseBox = ({ plan }) => {
     return (
-        <>
-            <Card sx={{ maxWidth: 345, marginTop: 1, marginBottom: 4, marginX: 1 }}>
-                <CardActionArea>
-                    <CardMedia
-                        sx={{ height: 140 }}
-                        image={course}
-                        title="green iguana"
-                    />
-                    <Stack direction="row" spacing={1} sx={{ margin: 2 }}>
-                        <Chip label="primary" color="primary" />
-                        <Chip label="success" color="success" />
-                    </Stack>
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Course Name
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores itaque delectus voluptas expedita tenetur id accusamus quas magni eaque. Ullam!
-                        </Typography>
-                    </CardContent>
-                    {/* <CardActions>
-                        <Button size="small">Share</Button>
-                        <Button size="small">Learn More</Button>
-                    </CardActions> */}
-                </CardActionArea>
-            </Card>
-        </>
-    )
+        <Card sx={{ maxWidth: 345, margin: '10px' }}>
+            <CardActionArea>
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image={courseImage}
+                    title={plan.title}
+                />
+                <Stack direction="row" spacing={1} sx={{ margin: 2 }}>
+                    <Chip label={plan.duration} color="primary" />
+                    <Chip label={`${plan.hoursPerWeek} hours/week`} color="success" />
+                </Stack>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {plan.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {plan.description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
 }
 
-export default CourseBox
+export default CourseBox;
--- src/Pages/Plans.jsx
+++ src/Pages/Plans.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CourseBox from '../Components/CourseBox';
 
 const LearningPlans = () => {
+    const [learningPlans, setLearningPlans] = useState([]);
+
+    useEffect(() => {
+        // Function to fetch the local JSON file
+        const fetchLearningPlans = async () => {
+            try {
+                // Assuming the JSON file is stored in the public directory
+                const response = await fetch('/plan.json');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                setLearningPlans(data);
+            } catch (error) {
+                console.error("Could not fetch the learning plans", error);
+            }
+        };
+
+        fetchLearningPlans();
+    }, []);
 
     return (
         <>
             <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'start', alignItems: 'end', height: 150 }}>
                 <h2 className='font2' style={{ marginLeft: '30%' }}>Learning Plans</h2>
             </div>
-            <p className='font2' style={{ marginLeft: '30%', width: '50%' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis temporibus velit provident qui suscipit. Minima fugit animi obcaecati eaque deleniti, repudiandae rerum blanditiis consequuntur, delectus dolores sapiente est. Obcaecati, sed!</p>
+            <p className='font2' style={{ marginLeft: '30%', width: '50%' }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis temporibus velit provident qui suscipit. Minima fugit animi obcaecati eaque deleniti, repudiandae rerum blanditiis consequuntur, delectus dolores sapiente est. Obcaecati, sed!
+            </p>
             <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-                <CourseBox style={{ flexBasis: '25%', flexGrow: 0, flexShrink: 0, maxWidth: '25%' }} />
+                {learningPlans.map((plan, index) => (
+                    <CourseBox key={index} plan={plan} />
+                ))}
             </div>
         </>
-
-    )
+    );
 }
 
-export default LearningPlans
+export default LearningPlans;
