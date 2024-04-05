a45679472d8e73b0e75d1a20e7a73704ce208a7a
mabgcm <bugucam@gmail.com>
2024-04-05 03:24:00 -0400

    learning plan structure

--- src/Pages/Plans.jsx
+++ src/Pages/Plans.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import CourseBox from '../Components/CourseBox';
 
 const LearningPlans = () => {
+
     return (
-        <h1 className='mt-5 pt-5'>LearningPlans</h1>
+        <>
+            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'start', alignItems: 'end', height: 150 }}>
+                <h2 className='font2' style={{ marginLeft: '30%' }}>Learning Plans</h2>
+            </div>
+            <p className='font2' style={{ marginLeft: '30%', width: '50%' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis temporibus velit provident qui suscipit. Minima fugit animi obcaecati eaque deleniti, repudiandae rerum blanditiis consequuntur, delectus dolores sapiente est. Obcaecati, sed!</p>
+            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+                <CourseBox style={{ flexBasis: '25%', flexGrow: 0, flexShrink: 0, maxWidth: '25%' }} />
+            </div>
+        </>
+
     )
 }
 
