6913986b383040a4968f897a9e1c5cacae4e2f26
mabgcm <bugucam@gmail.com>
2024-05-03 21:02:24 -0400

    lessondetails dome

--- src/Pages/LearningPlan.jsx
+++ src/Pages/LearningPlan.jsx
@@ -31,88 +31,42 @@ const LearningPlan = () => {
                     <Typography sx={{ marginX: '10px' }} variant="body1" gutterBottom>
                         {curriculum.description}
                     </Typography>
-                    <Box sx={{ width: '80%', marginBottom: '20px' }}>
-                        <Typography variant="h6" gutterBottom>Prerequisites:</Typography>
-                        <ul>
-                            {curriculum.prerequisites?.map((prerequisite, index) => (
-                                <li key={index}>{prerequisite}</li>
-                            ))}
-                        </ul>
-                        <Typography variant="h6" gutterBottom>Duration:</Typography>
-                        <p>{curriculum.duration}</p>
-                        <Typography variant="h6" gutterBottom>Level:</Typography>
-                        <p>{curriculum.level}</p>
-                        <Typography variant="h6" gutterBottom>Target Audience:</Typography>
-                        <p>{curriculum.targetAudience}</p>
-                        <Typography variant="h6" gutterBottom>Estimated Effort:</Typography>
-                        <p>{curriculum.estimatedEffort}</p>
-                        <Typography variant="h6" gutterBottom>Learning Outcomes:</Typography>
-                        <ul>
-                            {curriculum.learningOutcomes?.map((outcome, index) => (
-                                <li key={index}>{outcome}</li>
-                            ))}
-                        </ul>
-                        {Object.entries(curriculum.weeklyBreakdown || {}).map(([week, details], index) => (
-                            <Box key={index} sx={{ marginBottom: '20px' }}>
-                                <Typography variant="h4" gutterBottom>{details.week}</Typography>
-                                <Typography variant="h5" gutterBottom>{details.topic}</Typography>
-                                <Typography variant="h6" gutterBottom>Learning Objective:</Typography>
-                                <Typography variant="body1" gutterBottom>{details.objective}</Typography>
-                                <Typography variant="h6" gutterBottom>Resources:</Typography>
-                                <ol style={{ listStylePosition: 'inside', padding: 0, margin: 0 }}>
-                                    {Object.values(details.resources).map((resource, resourceIndex) => (
-                                        <li key={resourceIndex} sx={{ display: 'flex', alignItems: 'flex-start' }}>
-                                            {isVideoLink(resource.link) ? (
-                                                <iframe
-                                                    sx={{
-                                                        width: '100%', // Use 100% width for responsiveness
-                                                        height: { xs: '194px', sm: '315px' }, // Adjust height based on screen size
-                                                        maxWidth: '560px', // Set a max-width to constrain the video size on larger screens
-                                                        marginTop: '10px'
-                                                    }}
-                                                    src={resource.link.replace("watch?v=", "embed/")}
-                                                    title="Video player"
-                                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                                    allowFullScreen
-                                                ></iframe>
-                                            ) : (
-                                                <Chip
-                                                    label={resource.description}
-                                                    component="a"
-                                                    href={resource.link}
-                                                    clickable
-                                                    target="_blank"
-                                                    rel="noopener noreferrer"
-                                                    color="success"
-                                                    sx={{
-                                                        height: 'auto',
-                                                        '& .MuiChip-label': {
-                                                            display: 'block',
-                                                            whiteSpace: 'normal',
-                                                        },
-                                                        marginBottom: '10px'
-                                                    }}
-                                                />
-                                            )}
-                                        </li>
-                                    ))}
-                                </ol>
-                                <Typography variant="h6" gutterBottom>Activity of the week:</Typography>
-                                <Typography variant="body1" gutterBottom>{details.activities.activity1}</Typography>
-                                <Typography variant="h6" gutterBottom>Extra Sources:</Typography>
+                    {curriculum.weeks && curriculum.weeks.map((week, index) => (
+                        <Box sx={{ marginLeft: '10px', width: 'calc(100% - 40px)' }}>
+                            <Typography variant="h4" gutterBottom>Week {index + 1}: {week.title}</Typography>
+                            <Box sx={{ marginLeft: '40px' }}>
+                                <Typography variant="h6" gutterBottom>Study Hours:</Typography>
+                                <p>{week.studyHours}</p>
+                                <Typography variant="h6" gutterBottom>Learning Objectives:</Typography>
                                 <ul>
-                                    {details.interactiveElements?.map((element, elementIndex) => (
-                                        <li key={elementIndex}>
-                                            <Link href={element.link} target="_blank" rel="noopener noreferrer">{element.name}</Link>
-                                        </li>
+                                    {week.learningObjectives?.map((objective, objIndex) => (
+                                        <li key={objIndex}>{objective}</li>
                                     ))}
                                 </ul>
-                                {index !== Object.entries(curriculum.weeklyBreakdown || {}).length - 1 && (
-                                    <Divider sx={{ width: '100%', my: 2 }} />
-                                )}
+                                <Typography variant="h6" gutterBottom>Topics to Cover:</Typography>
+                                <ul>
+                                    {week.topicsToCover?.map((topic, topicIndex) => (
+                                        <li key={topicIndex}>{topic}</li>
+                                    ))}
+                                </ul>
+                                <Typography variant="h6" gutterBottom>Weekly Tasks:</Typography>
+                                <ul>
+                                    {week.weeklyTasks?.map((task, taskIndex) => (
+                                        <li key={taskIndex}>{task}</li>
+                                    ))}
+                                </ul>
+                                <Typography variant="h6" gutterBottom>Resources:</Typography>
+                                <ul>
+                                    {week.resources?.map((resource, resIndex) => (
+                                        <li key={resIndex}>{resource}</li>
+                                    ))}
+                                </ul>
                             </Box>
-                        ))}
-                    </Box>
+                            {index !== curriculum.weeks.length - 1 && (
+                                <Divider sx={{ width: '100%', my: 2 }} />
+                            )}
+                        </Box>
+                    ))}
                 </Box>
             </Grid>
             <Grid item xs={12} md={3} lg={3}>
